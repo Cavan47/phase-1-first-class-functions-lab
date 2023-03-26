645ccb942ada29d2d4a1e2516bde18524e3c5319
Cavan47 <31673728+Cavan47@users.noreply.github.com>
2023-03-26 10:54:59 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    const driversCopy = [drivers];
+    return [driversCopy[0][0], driversCopy[0][1]];
+    
+}
+
+const returnLastTwoDrivers = function(drivers){
+    const driversCopy = [drivers];
+    return [driversCopy[0][2], driversCopy[0][3]];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+
+   return function (y){
+        return x * y;
+   }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, firstOrLast){
+   const driverCopy = [drivers];
+    if (firstOrLast == returnFirstTwoDrivers){
+        return [driverCopy[0][0], driverCopy[0][1]];
+    }else if (firstOrLast == returnLastTwoDrivers){
+        return [driverCopy[0][2], driverCopy[0][3]];
+}
+}
